015459d3166b01de0b84db4cbb8738d43e51e4b6
Hara Chaitanya <harachaitanya@gmail.com>
2018-12-04 18:32:49 -0500

    Update day4.py

--- Day4/day4.py
+++ Day4/day4.py
@@ -21,34 +21,69 @@ def print_notes(notes):
 		print(note)
 
 def parse_notes(notes):
-	gaurd_sleep_times = defaultdict(list)
+	gaurd_sleep_times = {}
+	
 	gaurd = None
-	sleep = True
+	
 	for note in notes:
 		if re.search(r'[0-9]+', note[1]):
-			gaurd = re.findall(r'[0-9]+', note[1])[0]
+			gaurd = int(re.findall(r'[0-9]+', note[1])[0])
 		elif note[1].lower() == 'falls asleep':
-			sleep_time = note[0]
+			sleep_time = note[0].minute
 		elif note[1].lower() == 'wakes up':
-			wakeup_time = note[0]
-			tot_sleep_time = (wakeup_time - sleep_time).total_seconds() // 60
-			gaurd_sleep_times[gaurd].append(tot_sleep_time)
+			wakeup_time = note[0].minute
+			if gaurd in gaurd_sleep_times:
+				for i in range(sleep_time, wakeup_time):
+					gaurd_sleep_times[gaurd][i] += 1
+			else:
+				gaurd_sleep_times[gaurd] = [0 for _ in range(60)]
+				for i in range(sleep_time, wakeup_time):
+					gaurd_sleep_times[gaurd][i] += 1
 
-	
+	return gaurd_sleep_times
 
+def calc_part1(gaurd_sleep_times):
+	maxsleep = -1
+	maxgaurd = None
+	for gaurd in gaurd_sleep_times.keys():
+		sleepsum  = sum(gaurd_sleep_times[gaurd])
+		
+		
+		if maxsleep < sleepsum:
+			maxsleep = sleepsum
+			maxgaurd = gaurd
 
+	maxminute = gaurd_sleep_times[maxgaurd].index(max(gaurd_sleep_times[maxgaurd]))
+	print(maxminute, maxgaurd, maxgaurd * maxminute)
+	return maxgaurd * maxminute
 
+def calc_part2(gaurd_sleep_times):
+	maxsleep = -1
+	maxgaurd = None
+	for gaurd in gaurd_sleep_times.keys():
+		sleepmax  = max(gaurd_sleep_times[gaurd])
+		
+		
+		if maxsleep < sleepmax:
+			maxsleep = sleepmax
+			maxgaurd = gaurd
 
-
+	maxminute = gaurd_sleep_times[maxgaurd].index(max(gaurd_sleep_times[maxgaurd]))
+	print(maxminute, maxgaurd, maxgaurd * maxminute)
+	return maxgaurd * maxminute
 
 
 if __name__ == "__main__":
-	notes = read_file('day4-sample-input.txt')
+	notes = read_file('day4-input.txt')
 	print("*********Before Sort********")
 	print_notes(notes)
 	notes =  sort_notes(notes)
 	print("*********After Sort********")
 	print_notes(notes)
+	gaurd_sleep_times = parse_notes(notes)
+	print(gaurd_sleep_times)
+	print("part 1 - ID multiplied by minute is ", calc_part1(gaurd_sleep_times))
+	print("part 2 - ID multiplied by minute is ", calc_part2(gaurd_sleep_times))
 
 
 
